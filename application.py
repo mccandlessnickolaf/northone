2280b98ffa3565bbdbc0a780d073d74da19aecb7
mccandlessnickolaf <56041065+mccandlessnickolaf@users.noreply.github.com>
2020-04-15 12:25:44 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return redirect("https://zippyloan.com/", code=302)
+    return redirect("https://www.mb103.com/lnk.asp?o=17246&c=918273&a=279700&k=ECA558BF696C3C712A8D8B70D50EA035&l=18606", code=302)
